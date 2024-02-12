6297e7d18924a28e0fe3e001f4c819ec9c32cfd2
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-12 18:49:13 +0100

    UPDATE app-routing

--- sge/src/app/app-routing.module.ts
+++ sge/src/app/app-routing.module.ts
@@ -72,6 +72,7 @@ const routes: Routes = [
   { path: 'motivos-nodual', loadChildren: () => import('./motivos-nodual/motivos-nodual.module').then(m => m.MotivosNodualModule) },
   { path: 'unidades', loadChildren: () => import('./unidades/unidades.module').then(m => m.UnidadesModule) },
   { path: 'unidades-centros', loadChildren:() => import('./unidades-centro/unidades-centro.module').then(m => m.UnidadesCentroModule)},
+  { path: 'vacantes', loadChildren:() => import('./vacantes/vacantes.module').then(m => m.VacantesModule)},
 //  { path: 'ciclos-entidad', loadChildren: () => import('./entidades/datos-entidad/ciclos-entidad/ciclos-entidad.module').then(m => m.CiclosEntidadModule) },
 //  { path: 'contactos-entidad', loadChildren: () => import('./entidades/datos-entidad/contactos-entidad/contactos-entidad.module').then(m => m.ContactosEntidadModule) },
 //  { path: 'datos-basicos-entidad', loadChildren: () => import('./entidades/datos-entidad/datos-basicos-entidad/datos-basicos-entidad.module').then(m => m.DatosBasicosEntidadModule) },
