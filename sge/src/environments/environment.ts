8dac2ea32b8b54e6d237248830a9639fe63eaffe
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-12 19:42:52 +0100

    añadidos

--- sge/src/environments/environment.ts
+++ sge/src/environments/environment.ts
@@ -16,6 +16,7 @@ export const environment = {
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 export const URL_BASE = 'http://143.47.54.55/dev/app.radfpd';
+/* export const URL_BASE = 'http://143.47.48.57/dev'; */
 // export const URL_BASE = 'http://localhost/EditorialMonsalvez';
 export const URL_API = `${URL_BASE}/api/private`;
 //export const URL_IMG = `${URL_BASE}/api/private/`;
